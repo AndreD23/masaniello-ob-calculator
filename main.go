36b1df708c7541c0ef6ede56a3a7788d39df85f5
André Dorneles <andre@imperiosoft.com.br>
2024-11-20 18:05:15 -0300

    Add file writing functionality for Martingale results
    - Introduced 'os' package for file operations.
    - Created 'result.txt' to store the outcomes of the Martingale runs.
    - Implemented 'writeFile' function to log results based on assert checks.
    - Updated the main loop to include file writing instead of console output.
    - Ensures persistent logging of each operation's outcome for further analysis.

--- main.go
+++ main.go
@@ -3,32 +3,47 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 )
 
 func main() {
 	/**
 	 * Configs
 	 */
-	var qtdOps = 10
-	var totalMartingales = 1
-	var assertsStruct = [3]float32{60.0, 85.0, 95.0}
+	var qtdOps = 50                                  // Quantity of operations
+	var totalMartingales = 1                         // Total of martingales. 0 = Without martingale, 1 = Martingale 1, 2 = Martingale 2
+	var assertsStruct = [3]float32{60.0, 85.0, 95.0} // Asserts for Win, G1, G2. Ex: 60.0 = 60%. Above third value, is Loss
+
+	// Create file
+	f, err := os.Create("result.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
 	// Run Martingale
 	for i := 0; i < qtdOps; i++ {
 		// Get random number between 0 and 100
 		var random = getRandomNumber(0, 100)
-		fmt.Println("Random: ", random)
 
 		// Check asserts
 		var result = checkAssert(random, totalMartingales, assertsStruct[:])
-		fmt.Println(result)
+
+		// Write file
+		writeFile(f, result, totalMartingales)
 	}
 }
 
+/**
+ * Get random number between min and max
+ */
 func getRandomNumber(min, max float32) float32 {
 	return min + rand.Float32()*(max-min)
 }
 
+/*
+ * Check asserts
+ */
 func checkAssert(numAssert float32, martinGales int, asserts []float32) string {
 	if numAssert >= 0 && numAssert <= asserts[0] {
 		return "W"
@@ -43,4 +58,47 @@ func checkAssert(numAssert float32, martinGales int, asserts []float32) string {
 	}
 
 	return "L"
+}
+
+/**
+ * Write file based on result
+ */
+func writeFile(f *os.File, result string, martingales int) {
+	//fmt.Println("Recebido: ", result, martingales)
+
+	var content string
+
+	if result == "W" {
+		content = fmt.Sprintf("W\n")
+	}
+	if martingales == 1 {
+		if result == "G1" {
+			content = fmt.Sprintf("L\nW\n")
+		}
+
+		if result == "L" {
+			content = fmt.Sprintf("L\nL\n")
+		}
+	}
+	if martingales == 2 {
+		if result == "G1" {
+			content = fmt.Sprintf("L\nW\n")
+		}
+
+		if result == "G2" {
+			content = fmt.Sprintf("L\nL\nW\n")
+		}
+
+		if result == "L" {
+			content = fmt.Sprintf("L\nL\nL\n")
+		}
+	}
+	if content == "" {
+		content = fmt.Sprintf("L\n")
+	}
+
+	_, err := f.Write([]byte(content))
+	if err != nil {
+		panic(err)
+	}
 }
